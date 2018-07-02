23af77fb045cc785c628e9774672119ed281c6ff
Anna Vila <avilatus@redhat.com>
2018-07-02 15:41:19 +0200

    adds failover cache policy

---
+++ apicast-3.1.0/examples/custom-module/failover_cache.lua
@@ -0,0 +1,48 @@
+-- This module customizes the cache_handler. In case the proxy is not able to reach 
+-- the 3scale service management API, it will allow the traffic to go through to the
+-- API, only for a selected group of applications defined in a whitelist, 
+
+local whitelist = require "whitelist"
+
+local apicast = require('apicast').new()
+local proxy = require 'proxy'
+
+local _M = {
+  _VERSION = '3.0.1',
+  _NAME = 'failover cache policy in case 3scale cannot be reached'
+}
+
+local mt = { __index = setmetatable(_M, { __index = apicast }) }
+
+function _M.new()
+  return setmetatable({}, mt)
+end
+
+
+cache_handler.handlers.strict = function (cache, cached_key, response, ttl)
+  if response.status == 200 then
+    -- cached_key is set in post_action and it is in in authorize
+    -- so to not write the cache twice lets write it just in authorize
+    if ngx.var.cached_key ~= cached_key then
+      ngx.log(ngx.INFO, 'apicast cache write key: ', cached_key, ', ttl: ', ttl )
+      cache:set(cached_key, 200, ttl or 0)
+    end
+    return true
+
+  else if response.status == 504 then
+    log("Debug: timeout while trying to reach 3scale") 
+    local credentials = ngx.var.credentials 
+    if whitelist[credentials.app_id] then
+      ngx.log(ngx.INFO, 'apicast cache write key: ', cached_key, ', ttl: ', ttl )
+      cache:set(cached_key, 200, ttl or 0)
+    end
+    return true
+
+  else
+    ngx.log(ngx.NOTICE, 'apicast cache delete key: ', cached_key, ' cause status ', response.status)
+    cache:delete(cached_key)
+    return false, 'not authorized'
+  end
+end
+
+return _M
